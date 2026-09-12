94f02a155c83c9067ad89c19fb876a02af13882e
sarvesh singh <og.sarvesh1919@gmail.com>
2026-09-12 23:35:28 +0530

    Add files via upload

---
+++ love_calc.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("parent") ;
+form.addEventListener("submit" ,(e)=>{
+    e.preventDefault() ;
+    const boy = document.getElementById("boy") ;
+    const girl = document.getElementById("girl") ;
+    const l1 = boy.value.length ;
+    const l2 = girl.value.length ; 
+    const result = Math.pow(l1+l2,3)%101 ;
+    document.querySelector("h2").textContent= `result : ${result}%` ;
+    form.reset() ;
+})
